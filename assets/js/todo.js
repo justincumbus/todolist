c941bb568cb351955664b16fbc279bc27f3c6147
justin cumbus <justin.cumbus@gmail.com>
2017-05-04 13:58:02 +0000

    added ability to delete dynamically  added lis and strikethrough dynamically  added lis

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,20 +1,28 @@
-$("li").on("click", function( ) {
+$(document).on("click",".strikable", function( ) {
 console.log("test");
     $(this).toggleClass("struckthrough");
 });
 
-$(".deletable").on("click", function (event) { 
+$(document).on("click", '.deletable',function (event) { 
     
     $(this).parent().remove();
     event.stopPropagation();
     
     
+} );
+
+$(".deletable").on("click", function(event) {
+
+    $(this).parent().remove();
+    event.stopPropagation();
+
+
 });
 
 $("input").on("keypress", function (event) {
       if (event.which == 13) {
             console.log($("input").val());
-            $("ul").append("<li>" + $("input").val() + "</li>" );
+            $("ul").append("<li class=\"strikable\"> <span class=\"deletable\">x </span>"    + $("input").val() + "</li>" );
             $("input").val('');
             
           
