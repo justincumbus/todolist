5ebc23c4ede47da0dfe6e829d8d71bfd4fb6e48a
justin cumbus <justin.cumbus@gmail.com>
2017-05-04 03:11:25 +0000

    added core javascript to add li's to to-do's, remove li's from to-do's, and add styling to li's when clicked

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,1 +1,24 @@
-alert("Connected");
+$("li").on("click", function( ) {
+console.log("test");
+    $(this).toggleClass("struckthrough");
+});
+
+$(".deletable").on("click", function (event) { 
+    
+    $(this).parent().remove();
+    event.stopPropagation();
+    
+    
+});
+
+$("input").on("keypress", function (event) {
+      if (event.which == 13) {
+            console.log($("input").val());
+            $("ul").append("<li>" + $("input").val() + "</li>" );
+            $("input").val('');
+            
+          
+          
+      }
+    
+});
